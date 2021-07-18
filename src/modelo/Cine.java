e6fb28eaa2e9a17a60eaaaa6f5c28452df4cbf5f
ricardoCs7 <r.carrasco05@ufromail.cl>
2021-07-18 06:05:27 -0400

    Hacer una vistaCartelera que muestre todas las funciones Creadas ... Al entrar en modo cliente, debe mostrar las funciones para la pelicula seleccionada y en el boton ver toda la cartelera, muestra todas las funciones creadas.
    vista boleta debe tener la lista de las funciones, un spinner para el numero de asientos y que muestre el total.

--- src/modelo/Cine.java
+++ src/modelo/Cine.java
@@ -9,13 +9,16 @@ import vista.VistaInicio;
 
 /**
  *
- * @author Ricardo
+ * @author Ricardo Carrasco Sepulveda
  */
 public class Cine {
 
     /**
      * @param args the command line arguments
      */
+    
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
         VistaInicio vi = new VistaInicio();
